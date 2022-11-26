2a77e136b3c016c86207aaab5042ef61ce2b373a
daryamakavchik <darjamakavchik@gmail.com>
2022-11-25 19:07:00 -0800

    fixL string tests

--- cypress/e2e/string.cy.ts
+++ cypress/e2e/string.cy.ts
@@ -1,6 +1,5 @@
-import { DELAY_IN_MS } from "../../src/constants/delays";
-const url = 'http://localhost:3000';
-// export const cirleSelector = '[data-testid=circle]';
+import { DELAY_LONG } from '../../src/utils/constants';
+const url = "http://localhost:3000";
 
 describe("Строка отображается корректно", function () {
   before(function () {
@@ -13,30 +12,27 @@ describe("Строка отображается корректно", function ()
   });
 
   it("Строка отражается верно", function () {
-    const stringLength = 6;
-    const initialString = "string";
+    const stringLength = 5;
+    const initialString = "strin";
     const initialColors = [
       "rgb(210, 82, 225)",
       "rgb(0, 50, 255)",
       "rgb(0, 50, 255)",
       "rgb(0, 50, 255)",
-      "rgb(0, 50, 255)",
       "rgb(210, 82, 225)",
     ];
 
-    const changingString = "gnirts";
+    const changingString = "nirts";
     const changingColors = [
       "rgb(127, 224, 81)",
       "rgb(210, 82, 225)",
       "rgb(0, 50, 255)",
-      "rgb(0, 50, 255)",
       "rgb(210, 82, 225)",
       "rgb(127, 224, 81)",
     ];
 
-    const finalString = "gnirts";
+    const finalString = "nirts";
     const finalColors = [
-      "rgb(127, 224, 81)",
       "rgb(127, 224, 81)",
       "rgb(127, 224, 81)",
       "rgb(127, 224, 81)",
@@ -48,27 +44,23 @@ describe("Строка отображается корректно", function ()
     cy.get("button").should("not.be.disabled");
     cy.get("button[type='submit']").click();
 
-    cy.get('.circle').each(($el, index, $list) => {
+    cy.get("[data-testid=circle]").each(($el, index, $list) => {
       cy.get($list).should("have.length", stringLength);
       cy.get($el).contains(initialString[index]);
       cy.get($el).should("have.css", "border-color", initialColors[index]);
     });
 
-    cy.wait(DELAY_IN_MS);
+    cy.wait(DELAY_LONG);
 
-    cy.get('.circle').each(($el, index, $list) => {
+    cy.get("[data-testid=circle]").each(($el, index, $list) => {
       cy.get($list).should("have.length", stringLength);
       cy.get($el).contains(changingString[index]);
-      cy.get($el).should(
-        "have.css",
-        "border-color",
-        changingColors[index]
-      );
+      cy.get($el).should("have.css", "border-color", changingColors[index]);
     });
 
-    cy.wait(DELAY_IN_MS);
+    cy.wait(DELAY_LONG);
 
-    cy.get('.circle').each(($el, index, $list) => {
+    cy.get("[data-testid=circle]").each(($el, index, $list) => {
       cy.get($list).should("have.length", stringLength);
       cy.get($el).contains(finalString[index]);
       cy.get($el).should("have.css", "border-color", finalColors[index]);
